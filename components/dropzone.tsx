460a164373cfa14067babe47e6d84bc982610846
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-04-15 10:40:42 +0200

    Make drop actually work

--- components/dropzone.tsx
+++ components/dropzone.tsx
@@ -9,6 +9,7 @@ export type Props = {
 export default function Dropzone(props: Props) {
   let { onUpload, disabled } = props;
   let [ref, setRef] = React.useState<HTMLInputElement | null>(null);
+  let [dragging, setDragging] = React.useState<boolean>(false);
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (disabled) return;
@@ -30,17 +31,56 @@ export default function Dropzone(props: Props) {
     }
   };
 
+  const dragEnter = (e: React.DragEvent<HTMLInputElement>) => {
+    if (disabled) return;
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDragging(true);
+    }
+  };
+
+  const dragOver = (e: React.DragEvent<HTMLInputElement>) => {
+    if (disabled) return;
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const dragLeave = (e: React.DragEvent<HTMLInputElement>) => {
+    if (disabled) return;
+    e.stopPropagation();
+    e.preventDefault();
+
+    setDragging(false);
+  };
+
+  const drop = (e: React.DragEvent<HTMLInputElement>) => {
+    if (disabled) return;
+    e.stopPropagation();
+    e.preventDefault();
+
+    onUpload(e.dataTransfer.files[0]);
+  };
+
   return (
     <div
       className={classNames(
         "w-full border-dashed border-gray-400 text-gray-400 border-2 rounded",
         {
           "cursor-pointer hover:border-gray-500": !disabled,
+          "border-gray-500": dragging,
         }
       )}
       onClick={handleClick}
     >
-      <div className="w-full flex items-center justify-center h-40">
+      <div
+        className="w-full flex items-center justify-center h-40"
+        onDragEnter={dragEnter}
+        onDragOver={dragOver}
+        onDragLeave={dragLeave}
+        onDrop={drop}
+      >
         Click here or drag a file into this box to upload a file
       </div>
       <input
