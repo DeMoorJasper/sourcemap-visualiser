8cf5211091ad51d85cb2af5be1ca252a6ca333a6
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-04-15 12:58:48 +0200

    Refactor into Visualizer

---
+++ components/visualizer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { SourceMapType } from "../types";
+import SourceMap from "./sourcemap";
+
+export type Props = {
+  sourcemapContent: Array<SourceMapType>;
+};
+
+export default function Visualizer({ sourcemapContent }: Props) {
+  let [selectedSourceMapIndex, setSelectedSourceMapIndex] = React.useState(0);
+
+  if (sourcemapContent.length > 0) {
+    let selectedSourceMap = sourcemapContent[selectedSourceMapIndex];
+
+    return (
+      <div className="h-screen">
+        <div className="bg-gray-700 p-2 flex">
+          <div className="text-white mr-4">Select Bundle</div>
+          <select
+            onChange={(e) => {
+              setSelectedSourceMapIndex(parseInt(e.target.value, 10));
+            }}
+          >
+            {sourcemapContent.map((v, i) => {
+              return (
+                <option value={i} key={v.name}>
+                  {v.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <SourceMap sourcemap={selectedSourceMap} />
+      </div>
+    );
+  }
+}
